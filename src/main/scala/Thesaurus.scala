79ec5c6f5f486413e23d0b070c371ab5a04ee07f
Alex <ahansen2@trinity.edu>
2016-11-10 01:25:01 -0600

    initial commit

---
+++ src/main/scala/Thesaurus.scala
@@ -0,0 +1,25 @@
+import scala.io.Source
+
+object Thesaurus {
+  def main(args:Array[String]) {
+    println("The thesaurus is running")
+  }
+  def getSynonyms(inputWord:String, inputPart:String = ""):Array[String] = {
+    val entry = getThesaurusEntry(inputWord)
+    var toReturn:Array[String] = Array()
+    for (line <- entry) {
+      val splitLine = line.split('|')
+      val (part, designation, word) = (splitLine(0), splitLine(1), splitLine(2))
+      if (designation == "syn" ) {
+        if (inputPart != "") {
+          if (inputPart == part) { toReturn :+= word }
+        }
+        else toReturn :+= word
+      }
+    }
+    toReturn
+  }
+  def getThesaurusEntry(word:String): Array[String] = {
+    Source.fromURL("http://words.bighugelabs.com/api/2/9a0d1e46117e2bdb3bf6e1a1568faa3e/" + word + "/utf-8").mkString.split("\n")
+  }
+}
